e7f0268b7015d6359b202b62b95a510fcb69d354
leni8ec <leni8ec@gmail.com>
2024-11-18 21:54:55 +0300

    common: add `GameObjectsService` to create new objects

---
+++ Assets/CyberFactory/Code/Common/Services/GameObjects/GameObjectsService.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CyberFactory.Basics.Utils;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CyberFactory.Common.Services.GameObjects {
+    [UsedImplicitly]
+    public class GameObjectsService {
+
+        public const string DEFAULT_ROOT_PATH = "GameObjects";
+
+        private GameObject defaultRootGameObject;
+
+        /// Cache for created objects under default root
+        private readonly Dictionary<string, GameObject> defaultRootCache = new();
+        /// Cache for created custom root objects
+        private readonly Dictionary<string, GameObject> rootCache = new();
+        /// Temp variable root game objects from a default scene  
+        private readonly List<GameObject> rootGameObjectsBucket = new();
+
+        /// <summary>
+        /// Create new GameObject by path <br/>
+        /// (use 'ActiveScene' variable and 'transform.Find' to search path items)<br/><br/>
+        /// ⚠️ note: can be expensive on a huge number of calls!<br/>
+        /// </summary>
+        /// <param name="path">use '/' before path to specify scene root object, otherwise root default value will be used</param>
+        /// <param name="name">Name of GameObject</param>
+        /// <param name="components">Components on the game objects</param>
+        /// <returns></returns>
+        public GameObject Create(string path, string name = null, params Type[] components) {
+            if (string.IsNullOrEmpty(path)) {
+                Debug.LogError("Path is empty!");
+                return CreateInternal(name, components);
+            }
+
+            string[] paths = PathUtil.Split(path);
+            int pathShift = 0; // offset of the first created object in the path
+
+            // Find or create a root GameObject
+            GameObject parent;
+            string rootPart = paths[0];
+            bool isCustomRoot = rootPart.Length == 0; // custom root - if path started from '/' (it means 0 symbols before)
+            if (isCustomRoot) {
+                // path[0] - it's a delimiter,
+                // path[1] - it's a root object name
+                string rootName = paths[1];
+                parent = GetRoot(rootName); // use custom specified root
+                pathShift += 2;
+            } else {
+                parent = GetDefaultRoot(); // use predefined root variable
+                string parentName = paths[0];
+                if (defaultRootCache.TryGetValue(parentName, out var cachedRoot)) {
+                    if (cachedRoot) parent = cachedRoot;
+                    pathShift += 1;
+                }
+            }
+
+            // Find or Create full GameObject path
+            if (paths.Length >= 0 + pathShift) {
+                for (int i = 0 + pathShift; i < paths.Length; i++) {
+                    string part = paths[i];
+                    var transform = parent.transform.Find(part);
+                    if (!transform) transform = CreateInternal(parent, part).transform;
+                    parent = transform.gameObject;
+                    if (!isCustomRoot && i == pathShift) { // cache first parent for default root objects
+                        defaultRootCache[part] = transform.gameObject;
+                    }
+                }
+            }
+
+            // Create game object itself
+            return CreateInternal(parent, name, components);
+        }
+
+        public GameObject Create(string name = null, params Type[] components) {
+            return CreateInternal(GetDefaultRoot().transform, name, components);
+        }
+
+        private GameObject CreateInternal(GameObject parent, string name = null, params Type[] components) {
+            return CreateInternal(parent.transform, name, components);
+        }
+
+        private GameObject CreateInternal(Transform parent, string name = null, params Type[] components) {
+            var gameObject = CreateInternal(name, components);
+            gameObject.transform.SetParent(parent);
+            return gameObject;
+        }
+
+        private GameObject CreateInternal(string name = null, params Type[] components) {
+            if (name is { Length: 0 }) Debug.LogWarning("GameObject name is empty!");
+            GameObject gameObject;
+            if (components == null) gameObject = new GameObject(name);
+            else gameObject = new GameObject(name, components);
+            return gameObject;
+        }
+
+
+        /// <summary>
+        /// Get default root object
+        /// </summary>
+        /// <returns>predefined root object</returns>
+        private GameObject GetDefaultRoot() {
+            if (!defaultRootGameObject) defaultRootGameObject = new GameObject(DEFAULT_ROOT_PATH);
+            return defaultRootGameObject;
+        }
+
+        /// <summary>
+        /// Find for scene root objects or create new specified root
+        /// </summary>
+        /// <param name="rootObjectName">root object name</param>
+        /// <returns>already exists or newly created root object</returns>
+        private GameObject GetRoot(string rootObjectName) {
+            // Try get cached value
+            if (rootCache.TryGetValue(rootObjectName, out var cachedRoot)) {
+                if (cachedRoot) return cachedRoot;
+            }
+
+            // Try to find scene already exists object or create new
+            SceneManager.GetActiveScene().GetRootGameObjects(rootGameObjectsBucket);
+            var root = rootGameObjectsBucket.FirstOrDefault(go => go.name == rootObjectName);
+            if (!root) root = CreateInternal(rootObjectName);
+
+            // assign and clear variables
+            rootCache[rootObjectName] = root;
+            if (rootCache.Count > 100) Debug.LogWarning("RootCache has more than 100 objects!");
+            rootGameObjectsBucket.Clear();
+            return root;
+        }
+
+
+        public string GetPath(GameObject gameObject) {
+            return GetPath(gameObject.transform);
+        }
+
+        public string GetPath(Transform transform) {
+            string path = transform.name;
+            while (transform.parent) {
+                path = PathUtil.Combine(transform.parent.name, path, true);
+                transform = transform.parent;
+            }
+            return path;
+        }
+
+    }
+}
---
+++ Assets/CyberFactory/Code/Tests/Common/Services/GameObjectServiceTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using CyberFactory.Basics.Utils;
+using CyberFactory.Common.Services.GameObjects;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace CyberFactory.Tests.Common.Services {
+    public class GameObjectServiceTests {
+
+        private GameObjectsService service;
+
+        [SetUp]
+        public void SetUp() {
+            service = new GameObjectsService();
+        }
+
+        [Test]
+        [TestCase()]
+        [TestCase(null)]
+        [TestCase("null")]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("New object")]
+        public void GO_Create_Test(string name = null) {
+            if (name == null) name = "New Game Object";
+            var gameObject = service.Create(name);
+            Assert.IsNotNull(gameObject);
+            Assert.AreEqual(name, gameObject.name);
+        }
+
+        [Test]
+        [TestCase(null, null)]
+        [TestCase("null", "null")]
+        [TestCase("", "")]
+        [TestCase(" ", " ")]
+        [TestCase("New object", "New object")]
+        public void GO_Create_Multiple_Test(params string[] names) {
+            foreach (string name in names) {
+                GO_Create_Test(name);
+            }
+        }
+
+        [TestCase()]
+        [TestCase(typeof(SpriteRenderer), typeof(RectTransform))]
+        [TestCase(typeof(BoxCollider2D), typeof(Rigidbody2D))]
+        public void GO_Create_Components_Test(params Type[] components) {
+            var gameObject = service.Create(null, components);
+
+            foreach (var componentType in components) {
+                Assert.IsInstanceOf(componentType, gameObject.GetComponent(componentType));
+            }
+        }
+
+        [TestCase("parent/go")]
+        [TestCase("parent")]
+        [TestCase("parent/parent2/go")]
+        [TestCase("parent/parent2/go1/go2")]
+        [TestCase("parent/parent2/go1/go2/go3")]
+        [TestCase("sample/parent2/go1/go2/go3")]
+        [TestCase("/root")]
+        [TestCase("/sample")]
+        [TestCase("/sample/parent")]
+        public void Check_GO_Path_After_Create_GO_By_Path(in string path) {
+            const string gameObjectName = "gameObject";
+
+            string expectedPath;
+            if (PathUtil.IsRoot(path))
+                expectedPath = PathUtil.CombineForced(path.Substring(1, path.Length - 1), gameObjectName);
+            else
+                expectedPath = PathUtil.CombineForced(GameObjectsService.DEFAULT_ROOT_PATH, path, gameObjectName);
+
+            var gameObject = service.Create(path, gameObjectName);
+            string gameObjectPath = service.GetPath(gameObject);
+
+            Assert.AreEqual(expectedPath, gameObjectPath);
+
+            Debug.Log($"Expected path: {expectedPath}");
+            Debug.Log($"Actual path: {gameObjectPath}");
+        }
+
+
+        [TestCase("root")]
+        [TestCase("root", "go")]
+        [TestCase("root", "parent", "go")]
+        [TestCase("root", "parent", "go1", "go2")]
+        [TestCase("root", "parent", "go1", "go2", "go3")]
+        [TestCase("", "parent")]
+        [TestCase(" ", "parent")]
+        public void Check_GO_Path_After_Create_GO_By_Path_Parts(params string[] pathParts) {
+            string expectedPath = PathUtil.CombineForced(pathParts);
+
+            GameObject gameObject = null;
+            Transform parent = null;
+            foreach (string part in pathParts) {
+                gameObject = new GameObject(part);
+                if (parent) gameObject.transform.parent = parent;
+                parent = gameObject.transform;
+            }
+            string testPath = service.GetPath(gameObject);
+
+            Assert.AreEqual(expectedPath, testPath);
+
+            Debug.Log(expectedPath);
+            Debug.Log(testPath);
+        }
+
+    }
+}
