7a514df0730f701643f01298f98e238372043e7a
leni8ec <leni8ec@gmail.com>
2024-11-21 15:34:38 +0300

    feat: use factory pattern to create `ProductGhost` (#13)

---
+++ Assets/CyberFactory/Code/Products/Ghost/ProductGhostFactory.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading;
+using CyberFactory.Basics.Extensions;
+using CyberFactory.Common.Services.GameObjects;
+using Cysharp.Threading.Tasks;
+using Scellecs.Morpeh;
+using Scellecs.Morpeh.Providers;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using VContainer;
+using Timer = CyberFactory.Common.Timer.Timer;
+
+namespace CyberFactory.Products.Ghost {
+    public class ProductGhostFactory {
+
+        [Inject] private GameObjectsService GameObjectsService { get; init; }
+
+        public async UniTaskVoid CreateAsync(ProductGhostConfig config, AssetReferenceSprite spriteReference, Vector3 sourcePosition, CancellationToken token) {
+            // Create GameObject
+            var gameObject = GameObjectsService.Create("Ghosts", "ProductGhost",
+                typeof(EntityProvider), typeof(SpriteRenderer));
+
+            // todo: maybe use it later
+            // var gameObject = Object.InstantiateAsync(gameObject, 1, parent, sourcePosition, Quaternion.identity, token);
+
+            // Configure components
+            var sprite = await Addressables.LoadAssetAsync<Sprite>(spriteReference).WithCancellation(token);
+            var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = sprite;
+            spriteRenderer.SetColorAlpha(0); // predefine color value to avoid glitches
+            gameObject.transform.position = sourcePosition + config.startOffset;
+            gameObject.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f); // todo: scale (implement prefab usage)
+
+
+            // Create Entity
+            var entity = gameObject.GetComponent<EntityProvider>().Entity;
+            entity.AddComponent<ProductGhost>() = new ProductGhost {
+                // todo: async
+                spriteRenderer = spriteRenderer,
+                config = config,
+                transform = gameObject.transform,
+                sourcePosition = sourcePosition,
+            };
+            entity.AddComponent<Timer>() = new Timer() {
+                interval = config.duration
+            };
+        }
+
+    }
+}
---
+++ Assets/CyberFactory/Code/Products/Ghost/ProductGhostSpawnSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using CyberFactory.Basics.Constants.Editor;
+using CyberFactory.Basics.Extensions;
+using CyberFactory.Basics.Objects;
+using CyberFactory.Plants.Core.Components.View;
+using CyberFactory.Products.Events;
+using Scellecs.Morpeh;
+using Scellecs.Morpeh.Systems;
+using UnityEngine;
+using VContainer;
+
+namespace CyberFactory.Products.Ghost {
+    [CreateAssetMenu(menuName = AssetMenu.Plants.SYSTEM + "Product Ghost Create", order = AssetMenu.Plants.ORDER_VIEW)]
+    public sealed class ProductGhostSpawnSystem : Initializer {
+
+        [Inject] private ProductGhostFactory Factory { get; init; }
+
+        private DisposableTracker disposable;
+        private CancellationTokenSource tokenSource;
+
+        public override void OnAwake() {
+            disposable = new DisposableTracker();
+            tokenSource = new CancellationTokenSource().AddTo(disposable);
+
+            World.GetEvent<ProductCreatedEvent>().Subscribe(events => {
+                foreach (var e in events) {
+                    var plantView = e.plantEntity.GetComponent<PlantView>();
+                    var sourcePosition = plantView.transform.position;
+                    var ghostConfig = plantView.productGhostConfig;
+                    var spriteReference = e.product.icon;
+
+                    Factory.CreateAsync(ghostConfig, spriteReference, sourcePosition, tokenSource.Token).Forget();
+                }
+            }).AddTo(disposable);
+        }
+
+        public override void Dispose() {
+            tokenSource.Cancel();
+            disposable.Dispose();
+        }
+
+    }
+}
